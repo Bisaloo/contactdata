5b324f4dd637e24a24cd3911e7e01ee19c0ed3c6
Hugo Gruson <hugo.gruson@protonmail.com>
2021-02-18 12:40:57 +0100

    Create age_df_countries function

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# contactdata 0.2
+
+* Addition of a new function `age_df_countries()` to get population
+by age for all the countries for which we provide contact data.
+This is motivated by the fact that models using contact data by
+age will almost surely need population data by age as well.
+
 # contactdata 0.1
 
 * First release
---
+++ R/age_countries.R
@@ -0,0 +1,36 @@
+#' Get a data.frame (in long format) of population by age for multiple countries
+#'
+#' @inheritParams contact_df_countries
+#'
+#' @return A data.frame (in long format) with 3 columns:
+#'   * `country`: the country name
+#'   * `age`: the age group
+#'   * `pop`: the number of people in this age group
+#'
+#' @examples
+#' age_df_countries(c("Austria", "Belgium"))
+#'
+#' @export
+#'
+age_df_countries <- function(countries) {
+
+  pop_byage <- readRDS(
+    system.file("extdata", "population_byage.rds",
+                package = "contactdata")
+  )
+
+  unknown_countries <- countries[!countries %in% pop_byage$Country]
+
+  if (length(unknown_countries) != 0) {
+    warning(
+      "The following countries are not included in the dataset:\n",
+      paste0(unknown_countries, collapse = ", "), "\n",
+      "Use the list_countries() function to get a list of all ",
+      "countries in the dataset.",
+      call. = FALSE
+    )
+  }
+
+  pop_byage <- pop_byage[pop_byage$Country %in% countries, ]
+
+}
