8fd31044bb5d04a1d8f0ba4cc6720028ac61a4c1
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-01-24 15:16:37 +0100

    Split script in data-raw/ with one for each dataset

--- data-raw/contact_matrices_2017.R
+++ data-raw/contact_matrices_2017.R
@@ -68,20 +68,3 @@ for (l in locations) {
 }
 
 setwd(owd)
-
-library(dplyr)
-
-# From https://www2.census.gov/programs-surveys/international-programs/about/idb/idbzip.zip
-population_byage <- vroom::vroom("data-raw/idb5yr.all") %>%
-  filter(`#YR` == "2020") %>%
-  rename(country = NAME) %>%
-  select(country, matches("^POP\\d+\\_\\d+$")) %>%
-  mutate(country = fix_names(country)) %>%
-  filter(country %in% contactdata::list_countries()) %>%
-  rename_with(~ gsub("^POP", "", .x), matches("^POP\\d+\\_\\d+$")) %>%
-  rowwise() %>%
-  mutate("75+" = sum(`75_79`, `80_84`, `85_89`, `90_94`, `95_99`), .keep = "unused") %>%
-  tidyr::pivot_longer(-country, names_to = "age", values_to = "population") %>%
-  as.data.frame()
-
-saveRDS(population_byage, "inst/extdata/population_byage.rds")
