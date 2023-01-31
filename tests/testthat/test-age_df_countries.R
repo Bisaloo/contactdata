4bd0977af0cd0570c985ad6627d79599a8480df4
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-01-31 10:13:57 +0100

    Improve tests and docs for age_df_countries

--- tests/testthat/test-age_df_countries.R
+++ tests/testthat/test-age_df_countries.R
@@ -5,4 +5,12 @@ test_that("contact_df_age missing", {
     "not included"
   )
 
+  countries <- c("Austria", "Belgium")
+  age_df <- age_df_countries(countries)
+
+  expect_s3_class(age_df, "data.frame")
+  expect_identical(dim(age_df), c(16L*length(countries), 3L))
+  expect_named(age_df, c("country", "age", "population"))
+  expect_setequal(age_df$country, countries)
+
 })
