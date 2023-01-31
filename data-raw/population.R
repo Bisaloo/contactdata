373a2eac8ae55d665ee9c6ee7a8142f96b9782e0
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-01-31 12:53:54 +0100

    Add population data for new locations

--- data-raw/population.R
+++ data-raw/population.R
@@ -1,12 +1,26 @@
 library(dplyr)
 
+fix_names <- function(name) {
+
+  # Convert to common names in English
+  name <- stringi::stri_trans_general(name, "Latin-ASCII")
+  new_name <- countrycode::countryname(name)
+
+  return(new_name)
+}
+
+all_countries <- unique(c(
+  contactdata::list_countries(2017),
+  contactdata::list_countries(2020)
+))
+
 # From https://www2.census.gov/programs-surveys/international-programs/about/idb/idbzip.zip
 population_byage <- vroom::vroom("data-raw/idb5yr.all") %>%
   filter(`#YR` == "2020") %>%
   rename(country = NAME) %>%
   select(country, matches("^POP\\d+\\_\\d+$")) %>%
   mutate(country = fix_names(country)) %>%
-  filter(country %in% contactdata::list_countries()) %>%
+  filter(country %in% all_countries) %>%
   rename_with(~ gsub("^POP", "", .x), matches("^POP\\d+\\_\\d+$")) %>%
   rowwise() %>%
   mutate("75+" = sum(`75_79`, `80_84`, `85_89`, `90_94`, `95_99`), .keep = "unused") %>%
