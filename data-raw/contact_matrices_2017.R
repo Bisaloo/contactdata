c077de1a3e833a2ecd97491b9779d3ccb35958d7
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-02-01 10:18:20 +0100

    Improve data extraction script for 2017

--- data-raw/contact_matrices_2017.R
+++ data-raw/contact_matrices_2017.R
@@ -1,5 +1,14 @@
 owd <- setwd(tempdir())
 
+fix_names <- function(name) {
+
+  # Convert to common names in English
+  name <- stringi::stri_trans_general(name, "Latin-ASCII")
+  new_name <- countrycode::countryname(name)
+
+  return(new_name)
+}
+
 download.file(
   url = "https://doi.org/10.1371/journal.pcbi.1005697.s002",
   destfile = "all_datasets.zip"
@@ -37,7 +46,8 @@ for (l in locations) {
       file1,
       col_names = FALSE,
       sheet = c,
-      skip = 1
+      skip = 1,
+      .name_repair = "minimal"
     ))
     rownames(df) <- colnames(df) <- sprintf("%02i_%02i", seq(0, 75, 5), seq(5, 80, 5))
     c <- fix_names(c)
@@ -47,7 +57,8 @@ for (l in locations) {
     df <- as.matrix(read_xlsx(
       file2,
       col_names = FALSE,
-      sheet = c
+      sheet = c,
+      .name_repair = "minimal"
     ))
     rownames(df) <- colnames(df) <- sprintf("%02i_%02i", seq(0, 75, 5), seq(5, 80, 5))
     c <- fix_names(c)
