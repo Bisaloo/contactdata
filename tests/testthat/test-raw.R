31c08320dd860229feb3ed467bee11f917c1b738
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-02-01 14:47:16 +0100

    Update tests and args based on Kiesha's feedback

--- tests/testthat/test-raw.R
+++ tests/testthat/test-raw.R
@@ -22,12 +22,20 @@ test_that("All files have the correct lengths", {
   )
 
   for (l in locations) {
-    for (s in settings) {
+    for (s in c("all", "urban")) {
       f <- readRDS(
         system.file("extdata", paste0("contact_2020_", l, "_", s, ".rds"), package = "contactdata")
       )
      expect_length(f, 177)
     }
+
+    # 3 locations are missing because they don't have rural population (e.g.,
+    # Singapore, HK)
+    s <- "rural"
+    f <- readRDS(
+      system.file("extdata", paste0("contact_2020_", l, "_", s, ".rds"), package = "contactdata")
+    )
+    expect_length(f, 174)
   }
 
 })
